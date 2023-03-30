3fd4d8d6dee435f91976a21bc7b9572622db6b61
AdamIx <adamburdik9@gmail.com>
2023-03-30 17:49:03 +0200

    Calculator Added To projects/calculator route

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='templates/static')
 
 @app.route('/')
 def root():
     return render_template('index.html')
+
+@app.route('projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('projects/calculator')
+def projects():
+    return render_template('projects/calculator.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
